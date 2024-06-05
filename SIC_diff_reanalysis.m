cc42343a960d117c8d5c45f9d3370f60608ba862
ZhaohuiWang-imas <54881171+ZhaohuiWang-imas@users.noreply.github.com>
2024-06-05 23:12:56 +1000

    Create SIC_diff_reanalysis.m

---
+++ SIC_diff_reanalysis.m
@@ -0,0 +1,18 @@
+%% This code intercompared the SIC between different reanalyses
+
+% here we use 01-12-2013 to 30-11-2022 because JRA-3q has bug before this
+% time period
+clear
+%date of each experiment
+dates = [datenum('01-Dec-2013'):datenum('30-Nov-2020')];
+datestr = datestr(dates, 'yyyymmdd');
+datevec=datevec(dates);
+% plot a spatial pattern of climtology mean error of reanalyses in four season (DJF MAM JJA SON)
+% so this is a pattern plot 6 (reanalysis+sate) x 4 (seasons)
+x0=(1:length(datevec))';
+[x1,]=find(datevec(:,2)==3 | datevec(:,2)==1 | datevec(:,2)==2);
+[x2,]=find(datevec(:,2)==6 | datevec(:,2)==4 | datevec(:,2)==5);
+[x3,]=find(datevec(:,2)==9 | datevec(:,2)==7 | datevec(:,2)==8);
+[x4,]=find(datevec(:,2)==12 | datevec(:,2)==10 | datevec(:,2)==11);
+X={x0,x1,x2,x3,x4};
+
